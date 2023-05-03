7a664a2829ee60d12d6017e49632656debad36c3
apariciodevme <apariciodev@proton.me>
2023-05-03 15:54:18 -0300

    stack children

--- App.js
+++ App.js
@@ -4,18 +4,27 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 {
   /** Screens */
 }
 import HomeScreen from "./screens/HomeScreen";
 
+const Stack = createNativeStackNavigator();
+
+
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen />
+        <Stack.Navigator>
+      <Stack.Screen
+      name='HomeScreen'
+      component={HomeScreen}
+      />
+        </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
